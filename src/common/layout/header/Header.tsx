f305b49be7a9c2f3966e30145ff3939319c64526
0x7s0lt1 <zsoltfehervari@gmx.com>
2024-08-12 11:48:25 +0200

    card resize hide connect btn on header if not connected

--- src/common/layout/header/Header.tsx
+++ src/common/layout/header/Header.tsx
@@ -1,15 +1,18 @@
-import {FC} from "react";
+import { FC } from "react";
 import Link from "next/link";
-import {Web3ModalProvider} from "@/common/layout/Web3Modal";
+import { Web3ModalProvider } from "@/common/layout/Web3Modal";
+import { useWeb3ModalAccount } from '@web3modal/ethers/react';
 
 const Header : FC = () => {
+
+    const { address, chainId, isConnected } = useWeb3ModalAccount();
 
     return (
         <>
             <Web3ModalProvider/>
             <header className="header shadow">
                 <Link href={"/"} className="logo">
-                    &nbsp; BlockVault
+                    &nbsp; BlockVAULT
                 </Link>
                 {/*<nav>*/}
                 {/*    /!*<Link href={"/encrypt"}>*!/*/}
@@ -22,7 +25,7 @@ const Header : FC = () => {
                 {/*    /!*    Token*!/*/}
                 {/*    /!*</Link>*!/*/}
                 {/*</nav>*/}
-                <w3m-button balance="hide" size="md"/>
+                { isConnected && <w3m-button balance="hide" size="md"/> }
             </header>
         </>
     )
